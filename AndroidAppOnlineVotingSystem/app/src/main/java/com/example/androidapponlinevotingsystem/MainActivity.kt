3750732266d80f0c3bc1c9b0486e736cd30a38b7
iosifdaniel07 <iosifdaniel07@yahoo.com>
2022-03-02 09:56:48 +0200

    test2

--- AndroidAppOnlineVotingSystem/app/src/main/java/com/example/androidapponlinevotingsystem/MainActivity.kt
+++ AndroidAppOnlineVotingSystem/app/src/main/java/com/example/androidapponlinevotingsystem/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.androidapponlinevotingsystem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    var usingTablets:Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       usingTablets = resources.getBoolean(R.bool.using_dual_pane);
+
+        Toast.makeText(this, "Wide Screen? $usingTablets", Toast.LENGTH_LONG).show()
     }
+
 }
+
