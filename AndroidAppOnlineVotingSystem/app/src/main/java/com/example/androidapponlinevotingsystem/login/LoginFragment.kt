b888bbbe2e378463bef0c24b5058ddb68670c2b5
iosifdaniel07 <iosifdaniel07@yahoo.com>
2022-03-15 18:15:09 +0200

    testttttttt 2

--- AndroidAppOnlineVotingSystem/app/src/main/java/com/example/androidapponlinevotingsystem/login/LoginFragment.kt
+++ AndroidAppOnlineVotingSystem/app/src/main/java/com/example/androidapponlinevotingsystem/login/LoginFragment.kt
@@ -5,17 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import com.example.androidapponlinevotingsystem.R
 import com.example.androidapponlinevotingsystem.databinding.FragmentLoginBinding
+import com.example.androidapponlinevotingsystem.databinding.FragmentSignupBinding
 
 class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var text: TextView
+    private lateinit var loginBtn: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        loginBtn = binding.root.findViewById(R.id.btnLoggin)
+        text = binding.root.findViewById(R.id.textViewError)
+        text.visibility = View.INVISIBLE
+
+        loginBtn.setOnClickListener {
+            text.visibility = View.VISIBLE
+        }
+
+        return binding.root
     }
 
 }
