1ada720a235d7aa908f8d06118d051feed2cbac8
iosifdaniel07 <iosifdaniel07@yahoo.com>
2022-03-16 11:31:25 +0200

    item view test

--- AndroidAppOnlineVotingSystem/app/src/main/java/com/example/androidapponlinevotingsystem/login/LoginFragment.kt
+++ AndroidAppOnlineVotingSystem/app/src/main/java/com/example/androidapponlinevotingsystem/login/LoginFragment.kt
@@ -1,5 +1,6 @@
 package com.example.androidapponlinevotingsystem.login
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,7 +10,7 @@ import android.widget.Button
 import android.widget.TextView
 import com.example.androidapponlinevotingsystem.R
 import com.example.androidapponlinevotingsystem.databinding.FragmentLoginBinding
-import com.example.androidapponlinevotingsystem.databinding.FragmentSignupBinding
+import com.example.androidapponlinevotingsystem.main.MainActivity
 
 class LoginFragment : Fragment() {
 
@@ -17,6 +18,7 @@ class LoginFragment : Fragment() {
     private val binding get() = _binding!!
     private lateinit var text: TextView
     private lateinit var loginBtn: Button
+    private lateinit var forgotPasswordBtn: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,12 +27,18 @@ class LoginFragment : Fragment() {
 
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
 
+        forgotPasswordBtn = binding.root.findViewById(R.id.forgotPassword)
         loginBtn = binding.root.findViewById(R.id.btnLoggin)
         text = binding.root.findViewById(R.id.textViewError)
         text.visibility = View.INVISIBLE
 
         loginBtn.setOnClickListener {
             text.visibility = View.VISIBLE
+        }
+
+        forgotPasswordBtn.setOnClickListener{
+            val intent = Intent(activity,MainActivity::class.java)
+            startActivity(intent)
         }
 
         return binding.root
