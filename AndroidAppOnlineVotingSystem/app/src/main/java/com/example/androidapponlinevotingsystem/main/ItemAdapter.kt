2d05a2750f2e647b662db00979f1a52a5d23ff7f
iosifdaniel07 <iosifdaniel07@yahoo.com>
2022-03-16 11:31:25 +0200

    item view test

---
+++ AndroidAppOnlineVotingSystem/app/src/main/java/com/example/androidapponlinevotingsystem/main/ItemAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.androidapponlinevotingsystem.main
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemAdapter :RecyclerView.Adapter<ItemViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
+
+class ItemViewHolder(view:View) : RecyclerView.ViewHolder(view){
+
+    init {
+
+    }
+}
